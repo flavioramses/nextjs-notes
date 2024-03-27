55087e408d2d0eeda47ae01395086f68a4048b9d
Flavio Taccarelli <flaviotacca@hotmail.com>
2024-03-27 19:33:50 +0100

    fetch and show notes with axios

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import axios from "axios";
 
 export default function Home() {
+  let [notes, setNotes] = useState([]);
+  useEffect(function fetchNotes() {
+    console.log("Fetching notes with axios...");
+    axios
+      .get("http://localhost:8080/notes")
+      .then(function setNotesAfterFetch({ data }) {
+        setNotes(data.data);
+      });
+  }, []);
   return (
     <main className={styles.main}>
+      <h1>NextJS Notes 🗒️</h1>
       <div className={styles.description}>
-        <p>Hello Next.js</p>
+        {notes.map(function renderNotes(note, i) {
+          return <p key={`note${i}`}>{note?.title}</p>;
+        })}
       </div>
     </main>
   );
